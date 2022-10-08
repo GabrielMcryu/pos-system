5964334cfd34a36ada72c247ce5d6ebd3ee50acc
GabrielMcryu <gabrielmcryu@gmail.com>
2022-10-08 11:14:15 +0300

    Added a fix to the Search Customer/Product features

--- search_operations/search_customers.py
+++ search_operations/search_customers.py
@@ -6,26 +6,32 @@ filename = "storage/customers.json"
 def search_all_customers():
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    for customer_id in open_list:
-        name = open_list[customer_id]["name"]
-        phone_number = open_list[customer_id]["phone number"]
-        gender = open_list[customer_id]["gender"]
-        print(f"Customer ID: {customer_id}")
-        print(f"Customer Name: {name}")
-        print(f"Customer Phone number: 0{phone_number}")
-        print(f"Customer Gender: {gender}")
-        print("\n")
+    if not temp:
+        print("No Customer added into the system")
+    else:
+        [open_list] = temp
+        for customer_id in open_list:
+            name = open_list[customer_id]["name"]
+            phone_number = open_list[customer_id]["phone number"]
+            gender = open_list[customer_id]["gender"]
+            print(f"Customer ID: {customer_id}")
+            print(f"Customer Name: {name}")
+            print(f"Customer Phone number: 0{phone_number}")
+            print(f"Customer Gender: {gender}")
+            print("\n")
 
 
 # Lists all customer names
 def view_customer_names():
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    for customer_id in open_list:
-        name = open_list[customer_id]["name"]
-        print(f"Customer ID: {customer_id}, Customer Name: {name}")
+    if not temp:
+        print("No Customer added into the system")
+    else:
+        [open_list] = temp
+        for customer_id in open_list:
+            name = open_list[customer_id]["name"]
+            print(f"Customer ID: {customer_id}, Customer Name: {name}")
 
 
 # Lists one customer and their data
@@ -33,19 +39,22 @@ def search_one_customer():
     view_customer_names()
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    while True:
-        search_id = input("Enter Customer ID: ")
-        if search_id in open_list:
-            name = open_list[search_id]["name"]
-            phone_number = open_list[search_id]["phone number"]
-            gender = open_list[search_id]["gender"]
-            print(f"Customer name: {name}")
-            print(f"Customer Phone number: {phone_number}")
-            print(f"Customer Gender: {gender}")
-            break
-        else:
-            print("ID does not exist. Please input an existing ID")
+    if not temp:
+        print("No Customer added into the system")
+    else:
+        while True:
+            [open_list] = temp
+            search_id = input("Enter Customer ID: ")
+            if search_id in open_list:
+                name = open_list[search_id]["name"]
+                phone_number = open_list[search_id]["phone number"]
+                gender = open_list[search_id]["gender"]
+                print(f"Customer name: {name}")
+                print(f"Customer Phone number: {phone_number}")
+                print(f"Customer Gender: {gender}")
+                break
+            else:
+                print("ID does not exist. Please input an existing ID")
 
 
 # Lists Customer purchase history by ID
@@ -53,24 +62,27 @@ def view_customer_purchase_history():
     view_customer_names()
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    while True:
-        search_id = input("Enter Customer ID: \n")
-        if search_id in open_list:
-            for shopping_id in open_list[search_id]["Purchase History"]:
-                print(f"Shopping ID: {shopping_id}")
-                for item in open_list[search_id]["Purchase History"][shopping_id]:
-                    if item == "Total":
-                        total = open_list[search_id]["Purchase History"][shopping_id]["Total"]
-                        print(f"Total: {total} Kshs \n")
-                    else:
-                        product_name = open_list[search_id]["Purchase History"][shopping_id][item]["product name"]
-                        product_quantity = open_list[search_id]["Purchase History"][shopping_id][item]["product quantity"]
-                        product_cost = open_list[search_id]["Purchase History"][shopping_id][item]["product cost"]
-                        print(f"Name: {product_name}, Cost: {product_cost}, Quantity{product_quantity}")
-            break
-        else:
-            print("ID does not exist. Please input an existing ID")
+    if not temp:
+        print('No customers added into the system')
+    else:
+        [open_list] = temp
+        while True:
+            search_id = input("Enter Customer ID: \n")
+            if search_id in open_list:
+                for shopping_id in open_list[search_id]["Purchase History"]:
+                    print(f"Shopping ID: {shopping_id}")
+                    for item in open_list[search_id]["Purchase History"][shopping_id]:
+                        if item == "Total":
+                            total = open_list[search_id]["Purchase History"][shopping_id]["Total"]
+                            print(f"Total: {total} Kshs \n")
+                        else:
+                            product_name = open_list[search_id]["Purchase History"][shopping_id][item]["product name"]
+                            product_quantity = open_list[search_id]["Purchase History"][shopping_id][item]["product quantity"]
+                            product_cost = open_list[search_id]["Purchase History"][shopping_id][item]["product cost"]
+                            print(f"Name: {product_name}, Cost: {product_cost}, Quantity{product_quantity}")
+                break
+            else:
+                print("ID does not exist. Please input an existing ID")
 
 
 
--- search_operations/search_products.py
+++ search_operations/search_products.py
@@ -6,28 +6,34 @@ filename = "storage/products.json"
 def search_all_products():
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    for product_id in open_list:
-        name = open_list[product_id]["name"]
-        quantity = open_list[product_id]["quantity"]
-        price = open_list[product_id]["price"]
-        print(f"Product ID: {product_id}")
-        print(f"Product Name: {name}")
-        print(f"Product Quantity: {quantity}")
-        print(f"Product Price: {price}")
-        print('\n')
+    if not temp:
+        print("Products not added into the system")
+    else:
+        [open_list] = temp
+        for product_id in open_list:
+            name = open_list[product_id]["name"]
+            quantity = open_list[product_id]["quantity"]
+            price = open_list[product_id]["price"]
+            print(f"Product ID: {product_id}")
+            print(f"Product Name: {name}")
+            print(f"Product Quantity: {quantity}")
+            print(f"Product Price: {price}")
+            print('\n')
 
 
 # Lists all products and data in one line
 def view_product_names():
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    for product_id in open_list:
-        name = open_list[product_id]["name"]
-        quantity = open_list[product_id]["quantity"]
-        price = open_list[product_id]["price"]
-        print(f"ID: {product_id}, Name: {name}, Quantity: {quantity}, Price: {price} Kshs")
+    if not temp:
+        print("Products not added into the system")
+    else:
+        [open_list] = temp
+        for product_id in open_list:
+            name = open_list[product_id]["name"]
+            quantity = open_list[product_id]["quantity"]
+            price = open_list[product_id]["price"]
+            print(f"ID: {product_id}, Name: {name}, Quantity: {quantity}, Price: {price} Kshs")
 
 
 # List one product and its data
@@ -35,16 +41,19 @@ def search_one_product():
     view_product_names()
     with open(filename, "r") as f:
         temp = json.load(f)
-    [open_list] = temp
-    while True:
-        search_id = input("Enter Product ID: ")
-        if search_id in open_list:
-            name = open_list[search_id]["name"]
-            quantity = open_list[search_id]["quantity"]
-            price = open_list[search_id]["price"]
-            print(f"Product Name: {name}")
-            print(f"Product Quantity: {quantity}")
-            print(f"Product Price: {price}")
-            break
-        else:
-            print("ID does not exist. Please input an existing ID")
+    if not temp:
+        print("Products not added into the system")
+    else:
+        [open_list] = temp
+        while True:
+            search_id = input("Enter Product ID: ")
+            if search_id in open_list:
+                name = open_list[search_id]["name"]
+                quantity = open_list[search_id]["quantity"]
+                price = open_list[search_id]["price"]
+                print(f"Product Name: {name}")
+                print(f"Product Quantity: {quantity}")
+                print(f"Product Price: {price}")
+                break
+            else:
+                print("ID does not exist. Please input an existing ID")
